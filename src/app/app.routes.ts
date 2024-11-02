faef83b9f93031db5cf1b1fbe8695ec991e039af
GulliFerWD <clxl22@hotmail.com>
2024-11-02 17:24:49 -0600

    catalogo de peliculas para la nube

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { MoviesComponent } from './components/movies/movies.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'movies', component: MoviesComponent },
 ];
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -10,16 +10,9 @@ export class MoviesService {
 
   private http = inject(HttpClient);
 
-  //url para el servicio index
   private urlBase = environment.apiURL + '/api/all';
-  //private urlIndex = 'http://127.0.0.1:8000/api/all';
-  //private urlMovie = 'http://127.0.0.1:8000/api/movie/';
 
   public getMovies() {
     return this.http.get<any>(this.urlBase);
   }
-
-  /*public movieDescription(id: any) {
-    return this.http.get<any>(this.urlMovie + id);
-  }*/
 }
